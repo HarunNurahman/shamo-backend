76715b8a929148e586d75f59813b457839a00afc
Harun Nurahman <harun.nurrahman@hotmail.com>
2022-11-10 13:52:18 +0700

    Creating User Controller API (Login)

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -7,12 +7,14 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Laravel\Fortify\Rules\Password;
 
 class UserController extends Controller
 {
+    // Register Controller
     public function register(Request $request) {
         try {
             $validator = Validator::make($request->all(),
@@ -49,6 +51,40 @@ class UserController extends Controller
                 'message' => 'Something went wrong!',
                 'error' => $error
             ], 'Authentication failed', 500);
+        }
+    }
+
+    public function login(Request $request) {
+        try {
+            $request->validate([
+                'email' => 'email|required',
+                'password' => 'required',
+            ]);
+
+            $credentials = request(['email', 'password']);
+            if(!Auth::attempt($credentials)) {
+                return ResponseFormatter::error([
+                    'message' => 'Unauthorized'
+                ], 'Authentication Failed', 500);
+            }
+
+            $user = User::where('email', $request->email)->first();
+
+            if(!Hash::check($request->password, $user->password, [])){
+                throw new \Exception('Invalid Credentials');
+            }
+
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
+            return ResponseFormatter::success([
+                'access_token' => $tokenResult,
+                'token_type' => 'Bearer',
+                'user' => $user,
+            ], 'Authenticated');
+        } catch (Exception $error) {
+            return ResponseFormatter::error([
+                'message' => 'Something Went Wrong',
+                'error' => $error,
+            ], 'Authentication Failed', 500);
         }
     }
 }
